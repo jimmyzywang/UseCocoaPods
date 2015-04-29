6b761208d60ec60f42ba8aff96ae100ba8aa8b7d
jimmyzywang <jimmyzywang@qq.com>
2015-04-29 19:41:04 +0800

    init proj
    init proj

---
+++ Podfile
@@ -0,0 +1,3 @@
+source 'http://git.oschina.net/akuandev/Specs.git'
+platform :ios, '7.0'
+pod 'ASIHTTPRequest', '~> 1.8.2'
---
+++ UseCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UseCocoaPods
+//
+//  Created by jimmyzywang-nb on 14/12/3.
+//  Copyright (c) 2014年 com.JZWang.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
